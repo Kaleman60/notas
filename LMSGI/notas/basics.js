7bc0e46787cb7f423278ac3e02314b0ab38ca80a
katherine <kalemancardenas@gmail.com>
2025-11-10 10:49:27 +0100

    update

---
+++ LMSGI/notas/basics.js
@@ -0,0 +1,44 @@
+//variables y constantes
+
+let x = 10;// la variable puede cambiar su valor
+const y = 2; //la contante no puede cambiar su valor
+
+
+
+//Dato primitivos
+
+//String
+let string = "hola";
+
+
+//Number
+let entero = 30;
+
+
+//Boolean
+let esAdulto = true;
+
+
+//Float
+let altura = 1.75;
+
+//console.log(typeof(entero), typeof(altura))
+
+// Estructuras de datos
+
+let array = [12, 1.32, "hola", true ]; //arreglo o array
+//console.log(array[4])
+
+const user = {
+    nombre: "Juan",
+    edad: 34,
+    peliculasFavoritas: ["The matrix", "toy story"],
+    direccion: {
+        calle: "Calle Falsa 123",
+        numero: 123
+
+    }
+}
+
+console.log(user.peliculasFavoritas[0])
+console.log(user.direccion.calle)
