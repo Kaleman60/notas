e24cfc34f95f3f0386e66eb678bbaa8f3f4f4427
Kaleman60 <kalemancardenas@gmail.com>
2026-01-12 11:43:41 +0100

    update 11:43 am

--- LMSGI/funciones-anonimas/index.js
+++ LMSGI/funciones-anonimas/index.js
@@ -1,7 +1,20 @@
-const frutas = ["mango", "platano","manzana"]
+import {log} from "./utils.js"
+import { frutas, numeros, mezclas, usuarios} from "./utils.js"
 
-frutas.forEach((fruta, indice)=>{
-    console.log(fruta, indice)
+const frutasA = frutas.filter(fruta => { 
+    return fruta.endsWith("a")
 })
+console.log(frutasA)
 
-console.log(frutas)
+
+
+
+const frutasO = frutas.filter(fruta => fruta.endsWith("o"))
+console.log(frutasO)
+
+const negativos = numeros.filter(numeros => numeros < 0)
+console.log(numeros)
+console.log(negativos)
+
+const mezcla = mezclas.filter(mezcla => typeof mezcla == "string")
+console.log(mezcla)
--- LMSGI/funciones-anonimas/utils.js
+++ LMSGI/funciones-anonimas/utils.js
@@ -1,4 +1,4 @@
-function log(fn){
+export function log(fn){
     console.log("----------------")
     console.log("has ejecutado: ") 
     console.log(fn.toString())
@@ -6,8 +6,13 @@ function log(fn){
     console.log(fn())
 }
 
-function suma (a, b){
-    return a + b
-}
+export const frutas = ["mango", "platano", "manzana", "pera"]
+export const numeros = [1, 20, 32, -10, ,20, 1.23]
+export const mezclas = [undefined, null, 10, 1.23, "Hola mundo"]
+export const usuarios = [
+    {nombre: "Diego", edad: 35, genero: "M", saldo: 100},
+    {nombre: "Cecilia", edad: 25, genero: "F", saldo: 200},
+    {nombre: "Manolo", edad: 28, genero: "M", saldo: 150},
+    {nombre: "Maria", edad: 45, genero: "F", saldo: 800}
+]
 
-log(() => suma(1, 3))
