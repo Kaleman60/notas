0051231d3a302a3a10662017ba73b24df6075f9c
Kaleman60 <kalemancardenas@gmail.com>
2026-02-02 11:58:12 +0100

    se ha modificado en el index.ts y se creado un formulario en el index.html

--- LMSGI/inputs/index.ts
+++ LMSGI/inputs/index.ts
@@ -5,10 +5,16 @@ const port = 3000
 
 app.use(express.static("public"))
 app.use(express.urlencoded())
+app.use(express.json())
 
 app.post("/test", (req, res) =>{
     console.log(req.body)
-    res.redirect("/")
+    const respuesta ={
+        msg: "Datos recibidos correctamente",
+        playload: req.body
+    }
+    res.json(respuesta)
+    
 })
 
 app.listen(port, () => console.log(`Server is running on http://localhost:${port}`))
